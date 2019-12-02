5ea1346bcd24654c00746bd1f016b96968ca2839
nanashinogonbee <31703502+nanashinogonbee@users.noreply.github.com>
2019-12-02 16:15:13 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+    document.body.style.backgroundImage = "url(santa.png)";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundPosition = "33% 10%";
+
+    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+
+    function dateDiffInDays(a, b) {
+      const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+      const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+      return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+      }
+
+    let now_date = new Date();
+    let next_year = now_date.getFullYear() + 1;
+    let new_year = new Date('1/1/' + next_year);
+    let till_the_new_year = dateDiffInDays(now_date, new_year);
+    let days_word = null;
+    if([1].includes(till_the_new_year % 10) && till_the_new_year % 100 != 11){
+        days_word = 'день';
+        console.log('1');
+    } else if([2, 3, 4].includes(till_the_new_year % 10)){
+        days_word = 'дня';
+        console.log('2');
+    } else {
+        days_word = 'дней';
+        console.log('3');
+    }
+
+    let date = document.querySelector('.counter-time');
+    date.innerHTML = till_the_new_year + ' ' + days_word;
+    console.log();
+    }
