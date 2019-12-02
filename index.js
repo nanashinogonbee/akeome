36084a84c0b9087e8e4e12d167cb67b5096763db
nanashinogonbee <31703502+nanashinogonbee@users.noreply.github.com>
2019-12-02 17:32:41 +0300

    Text position fix

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ window.onload = function () {
     let scr_w = window.screen.width;
     let scr_h = window.screen.height;
 
-    document.body.style.backgroundImage = "url(santa.png)";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
-    document.body.style.backgroundPosition = '33% 10%';
-
+    document.body.style.backgroundImage = 'url(santa.png)';
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundAttachment = 'fixed';
+    document.body.style.backgroundPosition = 'top center';
 
     let content = document.querySelector('.content');
-    content.style.marginLeft = scr_w / 8 + 'px';
     content.style.marginTop = scr_h / 20 + 'px';
-    console.log(Math.floor(scr_h / 20));
-
-    // let header = document.querySelector('.counter-header');
-    // header.style.position = '70% 10%';
 
     const _MS_PER_DAY = 1000 * 60 * 60 * 24;
 
